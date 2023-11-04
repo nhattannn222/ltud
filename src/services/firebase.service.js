115895d40a6c83e8963de06a490af06bc37256a7
nhattannn222 <140130862+nhattannn222@users.noreply.github.com>
2023-11-05 00:53:51 +0700

    d

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,23 +1,23 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 class FirebaseService{
 async fcmBienDong(title,body,token){
-    const message = {
-        notification: {
-          title,
-          body,
-        },
-        token, // Mã thiết bị hoặc mã đăng ký FCM Token
-      };
-      try {
-        const response = await admin.messaging().send(message);
-        return response;
-      } catch (error) {
-        throw new AppError(201,error);
-      }
+    // const message = {
+    //     notification: {
+    //       title,
+    //       body,
+    //     },
+    //     token, // Mã thiết bị hoặc mã đăng ký FCM Token
+    //   };
+    //   try {
+    //     const response = await admin.messaging().send(message);
+    //     return response;
+    //   } catch (error) {
+    //     throw new AppError(201,error);
+    //   }
 }
 }
 const firebaseService=new FirebaseService();
