2528eb3539512f9cb7cd1ff1442191b43792cf92
nhattannn222 <140130862+nhattannn222@users.noreply.github.com>
2023-11-05 01:28:54 +0700

    d

--- src/index.js
+++ src/index.js
@@ -19,30 +19,62 @@ const v1=require("./routers/v1");
 const authorization = require("./middlewares/authorization");
 app.use("/api/v1",v1);
 
-
+function getAccessToken() {
+  return new Promise(function(resolve, reject) {
+    const key = require('./ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
+    const jwtClient = new google.auth.JWT(
+      key.client_email,
+      null,
+      key.private_key,
+      SCOPES,
+      null
+    );
+    jwtClient.authorize(function(err, tokens) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(tokens.access_token);
+    });
+  });
+}
 const admin = require('firebase-admin');
 const serviceAccount = require('./ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+const axios = require('axios');
 app.post("/send-notification", async (req, res,next) => {
-  const message = {
-    data: {
-    score: '850',
-    time: '2:45'
-    },
-    token: 'ebrA_p6PRo-R7v1MoEEEpt:APA91bHYGJHjMdEzN8y-DW3hOBwR4R2c7p_MLq79u71McTcYvSr0tseJKPv45-aV-dOvTtwLLpub_l3FLodqA7fGbIVlxbk6ABhWcCYv3yf5Wu2bbuZJ5a0VocOMDDLm6aIKArZNeNbi', // Mã thiết bị hoặc mã đăng ký FCM Token
-  };
+ 
+const apiUrl = 'https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send';
+const authToken = `Bearer ${getAccessToken()}`; // Thay thế bằng mã xác thực Firebase của bạn
 
-  try {
-    const response = await admin.messaging().send(message);
-    console.log('Thông báo đã được gửi:', response);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error('Gửi thông báo thất bại:', error);
-    // res.status(500).json({ success: false });
-    next(error);
-  }
+const requestData = {
+  message: {
+    token: 'ebrA_p6PRo-R7v1MoEEEpt:APA91bHYGJHjMdEzN8y-DW3hOBwR4R2c7p_MLq79u71McTcYvSr0tseJKPv45-aV-dOvTtwLLpub_l3FLodqA7fGbIVlxbk6ABhWcCYv3yf5Wu2bbuZJ5a0VocOMDDLm6aIKArZNeNbi', // Thay thế bằng token của thiết bị đích
+    notification: {
+      body: 'This is an FCM notification message!',
+      title: 'FCM Message',
+    },
+  },
+};
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': authToken,
+  },
+};
+
+axios.post(apiUrl, requestData, config)
+  .then((response) => {
+    console.log('API response:', response.data);
+  })
+  .catch((error) => {
+    console.error('API error:', error);
+  });
+
+  
 });
 app.use(handelErrors);
   
