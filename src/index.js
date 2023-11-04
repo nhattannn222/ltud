14da0a8f9d8adf606c6dcb001804eee9353f705b
nhattannn222 <140130862+nhattannn222@users.noreply.github.com>
2023-11-05 02:26:07 +0700

    .

--- src/index.js
+++ src/index.js
@@ -26,40 +26,40 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const { respone } = require("./helpers/respones");
-app.get("/send-notification", async (req, res,next) => {
+// const { respone } = require("./helpers/respones");
+// app.get("/send-notification", async (req, res,next) => {
  
-try {
+// try {
 
-  const message = {
-    notification: {
-      title: '`$FooCorp` up 1.43% on the day',
-      body: 'FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.'
-    },
-    android: {
-      notification: {
-        icon: 'stock_ticker_update',
-        color: '#7e55c3'
-      }
-    },
-    token: 'ebrA_p6PRo-R7v1MoEEEpt:APA91bHYGJHjMdEzN8y-DW3hOBwR4R2c7p_MLq79u71McTcYvSr0tseJKPv45-aV-dOvTtwLLpub_l3FLodqA7fGbIVlxbk6ABhWcCYv3yf5Wu2bbuZJ5a0VocOMDDLm6aIKArZNeNbi',
-  };
+//   const message = {
+//     notification: {
+//       title: '`$FooCorp` up 1.43% on the day',
+//       body: 'FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.'
+//     },
+//     android: {
+//       notification: {
+//         icon: 'stock_ticker_update',
+//         color: '#7e55c3'
+//       }
+//     },
+//     token: 'ebrA_p6PRo-R7v1MoEEEpt:APA91bHYGJHjMdEzN8y-DW3hOBwR4R2c7p_MLq79u71McTcYvSr0tseJKPv45-aV-dOvTtwLLpub_l3FLodqA7fGbIVlxbk6ABhWcCYv3yf5Wu2bbuZJ5a0VocOMDDLm6aIKArZNeNbi',
+//   };
   
-  admin.messaging().send(message)
-    .then((response) => {
-      console.log('Successfully sent message:', response);
-      res.status(200).json(respone(response));
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error);
-      throw error
-    });
-} catch (error) {
-  next(error)
-}
+//   admin.messaging().send(message)
+//     .then((response) => {
+//       console.log('Successfully sent message:', response);
+//       res.status(200).json(respone(response));
+//     })
+//     .catch((error) => {
+//       console.log('Error sending message:', error);
+//       throw error
+//     });
+// } catch (error) {
+//   next(error)
+// }
 
   
-});
+// });
 app.use(handelErrors);
   
 app.listen(4000);
--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,23 +1,37 @@
-// const admin = require('firebase-admin');
-// const serviceAccount = require('../ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+const admin = require('firebase-admin');
+const serviceAccount = require('../ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 class FirebaseService{
 async fcmBienDong(title,body,token){
-    // const message = {
-    //     notification: {
-    //       title,
-    //       body,
-    //     },
-    //     token, // Mã thiết bị hoặc mã đăng ký FCM Token
-    //   };
-    //   try {
-    //     const response = await admin.messaging().send(message);
-    //     return response;
-    //   } catch (error) {
-    //     throw new AppError(201,error);
-    //   }
+    try {
+        const message = {
+          notification: {
+            title,
+            body,
+          },
+          android: {
+            notification: {
+              icon: 'stock_ticker_update',
+              color: '#7e55c3'
+            }
+          },
+          token,
+        };
+        
+        admin.messaging().send(message)
+          .then((response) => {
+            console.log('Successfully sent message:', response);
+            res.status(200).json(respone(response));
+          })
+          .catch((error) => {
+            console.log('Error sending message:', error);
+            throw error
+          });
+      } catch (error) {
+        next(error)
+      }
 }
 }
 const firebaseService=new FirebaseService();
