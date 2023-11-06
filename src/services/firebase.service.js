d20ae895242d9b846a0cd9b2d78e95aa1de00fed
nhattannn222 <140130862+nhattannn222@users.noreply.github.com>
2023-11-06 16:53:55 +0700

    .

--- src/services/firebase.service.js
+++ src/services/firebase.service.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const { json } = require('sequelize');
 const serviceAccount = require('../ltud-1268c-firebase-adminsdk-nmczk-026bb7c720.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -9,7 +10,7 @@ async fcmBienDong(title,body,token,tkNhan){
     try {
         const message = {
           data: {
-            tkNhan,
+            tkNhan:JSON.stringify(tkNhan),
           },
           notification: {
             title:title,
