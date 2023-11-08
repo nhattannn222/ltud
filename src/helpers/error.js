9d77caa0af3ab677916cabf06c8792b8d6bcc1af
nhattannn222 <140130862+nhattannn222@users.noreply.github.com>
2023-11-08 15:43:04 +0700

    .

--- src/helpers/error.js
+++ src/helpers/error.js
@@ -6,7 +6,7 @@ class AppError extends Error{
 }
 const handelErrors=(err,req,res,next)=>{
    if(!(err instanceof AppError)){
-     err= new AppError(500,"hệ thống đang bảo trì");
+     err= new AppError(500,err.message);
    }
    const {message,statusCode}=err;
    res.status(statusCode).json({
